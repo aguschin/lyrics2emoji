ce5323c07edaaaebbb878d1e65ad25832c9dcc40
Nicolas <nicolas.parucker@hotmail.com>
2023-10-10 21:22:19 +0700

    hooked the game up with the data

--- src/game_state.py
+++ src/game_state.py
@@ -1,10 +1,13 @@
 from random import choice
+import streamlit as st
+from pandas import DataFrame
+from pandas import read_csv
 from text_to_emoji import translate_text
 
 
 class GameState:
     def __init__(self) -> None:
-        self.options: list[str] = get_options([])
+        self.options: list[str] = get_options()
         self.correct_option: str = choice(self.options)
         self.correct_songs: list[str] = []
         self.score: int = 0
@@ -13,7 +16,7 @@ class GameState:
     def next_level(self, option: str) -> None:
         self.score += 1
         self.correct_songs.append(option)
-        self.options = get_options(self.correct_songs)
+        self.options = get_options()
         self.correct_option = choice(self.options)
 
     def end_game(self) -> None:
@@ -22,7 +25,7 @@ class GameState:
     def reset(self) -> None:
         self.game_over = False
         self.score = 0
-        self.options = get_options([])
+        self.options = get_options()
         self.correct_option = choice(self.options)
 
     def get_correct_option_emoji(self) -> str:
@@ -34,5 +37,12 @@ class GameState:
                f" {self.score} \ngame over: {self.game_over}"
 
 
-def get_options(used_options: list[str]) -> list[str]:
-    return ["laugh", "heart", "smile"]
+@st.cache_data
+def get_data() -> DataFrame:
+    data = read_csv("data/sample_data/top_10_artists_songs.csv", nrows=1000)
+    return data
+
+
+def get_options() -> list[str]:
+    data: DataFrame = get_data()
+    return data.sample(n=3).song_name.values
