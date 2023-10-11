f6ab66c6cd0d4d0c7a8d2fec6b81017b3d4529e0
Anton Topchiy <topchiy.anton@gmail.com>
2023-10-11 13:39:37 +0700

    Everything is better with Path

--- src/batch_translate_text.py
+++ src/batch_translate_text.py
@@ -1,5 +1,7 @@
 import json
+from pathlib import Path
 from sys import argv
+from typing import List
 
 from text_to_emoji import translate_text
 
@@ -11,13 +13,15 @@ def batch_translate_texts(texts):
 
 def load_texts_from_file(filename):
     """Returns list of texts from json file."""
-    with open(filename, 'r') as f:
+    file = Path(filename)
+    with open(file, 'r') as f:
         texts = json.load(f)
     return texts
 
 
-def save_texts_to_file(texts, original_filename: str):
-    with open(f'{original_filename.lstrip(".json")}_translated.json', 'w') as f:
+def save_texts_to_file(texts: List[str], original_filename: str):
+    filepath = Path(f'{original_filename.lstrip(".json")}_translated.json')
+    with open(filepath, 'w') as f:
         json.dump(texts, f)
 
 
@@ -25,6 +29,7 @@ def translate_given_json_file(filename):
     texts = load_texts_from_file(filename)
     translated_texts = batch_translate_texts(texts)
     save_texts_to_file(translated_texts, filename)
+
 
 if __name__ == '__main__':
     translate_given_json_file(argv[1])
