77a0e1132d7c1b77cd4a54bcf859ed4f833f45bb
Anton Topchiy <topchiy.anton@gmail.com>
2023-10-11 13:57:20 +0700

    Change format, add title

--- src/batch_translate_text.py
+++ src/batch_translate_text.py
@@ -1,33 +1,43 @@
 import json
+from dataclasses import dataclass
 from pathlib import Path
 from sys import argv
-from typing import List
+from typing import List, Dict
 
 from text_to_emoji import translate_text
 
 
-def batch_translate_texts(texts):
+@dataclass
+class Song:
+    title: str
+    text: str
+
+
+def batch_translate_texts(songs: List[Song]):
     """Returns list of texts translated to emojis."""
-    return [translate_text(text) for text in texts]
+    res = []
+    for song in songs:
+        res.append({'title': song.title, 'text': translate_text(song.text)})
+    return res
 
 
 def load_texts_from_file(filename):
     """Returns list of texts from json file."""
     file = Path(filename)
     with open(file, 'r') as f:
-        texts = json.load(f)
-    return texts
+        items = json.load(f)
+    return [Song(text=song['text'], title=song['title']) for song in items if song.get('text')]
 
 
-def save_texts_to_file(texts: List[str], original_filename: str):
+def save_texts_to_file(texts: List[Dict], original_filename: str):
     filepath = Path(original_filename.replace('.json', '_translated.json'))
     with open(filepath, 'w') as f:
         json.dump(texts, f)
 
 
 def translate_given_json_file(filename):
-    texts = load_texts_from_file(filename)
-    translated_texts = batch_translate_texts(texts)
+    songs = load_texts_from_file(filename)
+    translated_texts = batch_translate_texts(songs)
     save_texts_to_file(translated_texts, filename)
 
 
