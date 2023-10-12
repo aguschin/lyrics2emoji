e16d15f91176bfc8afd6054be2321c36fbcb242e
v.kotunov <purgen200@gmail.com>
2023-10-12 15:19:20 +0700

    added average position's measure in list of neighbours; normalized AnnoyIndex distance

--- src/song_search.py
+++ src/song_search.py
@@ -17,9 +17,9 @@ list of first n songs with closest embeddings to the input
 """
 
 # lyrics source and column of song name
-df = pd.read_csv('data/sample_data/top_10_artists_songs.csv')
+df = pd.read_csv('../data/sample_data/top_10_artists_songs.csv')
 song_col = 'song_name'
-vectorized_lyrics = np.load('data/sample_data/lyrics_vectorized.npy')
+vectorized_lyrics = np.load('../data/sample_data/lyrics_vectorized.npy')
 
 # parser, model, tokenizer
 tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
@@ -27,10 +27,12 @@ model = DistilBertModel.from_pretrained("distilbert-base-uncased")
 embedding_dimension = model.config.hidden_size
 
 # create annoy index for faster search
-annoy_index = AnnoyIndex(embedding_dimension, 'euclidean')
-for i, vectorized_lyric in enumerate(vectorized_lyrics):
-    annoy_index.add_item(i, vectorized_lyric)
-annoy_index.build(10)
+annoy_index = AnnoyIndex(embedding_dimension, 'dot')
+for i, vector in enumerate(vectorized_lyrics):
+    vector = vector.squeeze()
+    vector = vector / np.linalg.norm(vector)
+    annoy_index.add_item(i, vector)
+annoy_index.build(20)
 
 def lyrics_pre_process(lyrics: str):
     return lyrics.split('\n')[0].replace('\r', '')
@@ -38,8 +40,11 @@ def lyrics_pre_process(lyrics: str):
 def embed(value: str):
     encoded_input = tokenizer(value, return_tensors='pt')
     output = model(**encoded_input)
-    return output.last_hidden_state.squeeze(0)[-1].detach().numpy().reshape(1, -1)
+    res = output.last_hidden_state.squeeze(0)[-1].detach().numpy().reshape(1, -1)
+
+    res = res / np.linalg.norm(res)
+    return res
 
 def find_nearest_song_annoy(emojis, n=1):
-    idx = annoy_index.get_nns_by_vector(embed(emojis).reshape(-1,1), n)
+    idx, dist = annoy_index.get_nns_by_vector(embed(emojis).reshape(-1,1), n, include_distances=True)
     return list(df[song_col].iloc[idx])
