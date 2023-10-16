59742c545c10a4febe6204cd744de138819a7492
v.kotunov <purgen200@gmail.com>
2023-10-16 14:44:02 +0700

    complete test in notebook for emoji seq

--- src/song_search.py
+++ src/song_search.py
@@ -17,14 +17,31 @@ list of first n songs with closest embeddings to the input
 """
 
 # lyrics source and column of song name
-df = pd.read_csv('../data/sample_data/top_10_artists_songs.csv')
+df = pd.read_json('../data/sample_data/top_10_spotify_translated.json')
 song_col = 'song_name'
-vectorized_lyrics = np.load('../data/sample_data/lyrics_vectorized.npy')
 
 # parser, model, tokenizer
 tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 model = DistilBertModel.from_pretrained("distilbert-base-uncased")
 embedding_dimension = model.config.hidden_size
+
+def get_sub_lyrics(lyrics):
+    return lyrics[:min(800, len(lyrics))].strip().replace('\r', '').replace('\n', ' ')
+
+def embed(value: str):
+    encoded_input = tokenizer(value, return_tensors='pt')
+    output = model(**encoded_input)
+    res = output.last_hidden_state.squeeze(0)[-1].detach().numpy().reshape(1, -1)
+
+    res = res / np.linalg.norm(res)
+    return res
+
+df['lyrics'] = df['lyrics'].apply(get_sub_lyrics)
+
+vectorized_lyrics = []
+for text in df['lyrics'].values:
+    vectorized_lyrics.append(embed(text))
+
 
 # create annoy index for faster search
 annoy_index = AnnoyIndex(embedding_dimension, 'dot')
@@ -36,14 +53,6 @@ annoy_index.build(20)
 
 def lyrics_pre_process(lyrics: str):
     return lyrics.split('\n')[0].replace('\r', '')
-
-def embed(value: str):
-    encoded_input = tokenizer(value, return_tensors='pt')
-    output = model(**encoded_input)
-    res = output.last_hidden_state.squeeze(0)[-1].detach().numpy().reshape(1, -1)
-
-    res = res / np.linalg.norm(res)
-    return res
 
 def find_nearest_song_annoy(emojis, n=1):
     idx, dist = annoy_index.get_nns_by_vector(embed(emojis).reshape(-1,1), n, include_distances=True)
